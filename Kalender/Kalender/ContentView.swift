064f1944f33c781e48d6d476668c37a0af8df18d
Elias Breitenbach <elias.b@me.com>
2024-03-09 21:24:53 +0100

    create a project

---
+++ Kalender/Kalender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kalender
+//
+//  Created by Elias Breitenbach on 09.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kalender/Kalender/KalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalenderApp.swift
+//  Kalender
+//
+//  Created by Elias Breitenbach on 09.03.24.
+//
+
+import SwiftUI
+
+@main
+struct KalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
